62feebd5c8687e6ea210a9ec3c4ffbee89f5577e
lisa.leonowa@yandex.ru <lisa.leonowa@yandex.ru>
2023-04-11 16:20:46 +0300

    Update: formsClients.py, viewsClients.py.

--- project/appClients/formsClients.py
+++ project/appClients/formsClients.py
@@ -9,14 +9,21 @@ class NewClients(forms.Form):
     phone = forms.CharField(label="Номер телефона:", max_length=12, min_length=10)
     remark = forms.CharField(label="Примечание:", required=False, max_length=250)
 
+    def form_values(self):  # метод возвращающий поля формы
+        return ['first_name', 'last_name', 'full_name', 'gender', 'phone', 'remark']
+
 
 class UpdateClients(forms.Form):
     phone = forms.CharField(label="Номер делефона:", max_length=12, min_length=10)
     remark = forms.CharField(label="Примечание:", required=False, max_length=250)
+    def form_values(self):  # метод возвращающий поля формы
+        return ['phone', 'remark']
 
 
 class SearchClients(forms.Form):
     first_name = forms.CharField(label="Имя:", min_length=5, max_length=70)
     last_name = forms.CharField(label="Фамилия:", min_length=5, max_length=70)
     full_name = forms.CharField(label="Отчество:", min_length=5, max_length=70)
+    def form_values(self):  # метод возвращающий поля формы
+        return ['first_name', 'last_name', 'full_name']
 
--- project/appClients/viewsClients.py
+++ project/appClients/viewsClients.py
@@ -1,27 +1,34 @@
-from django.http import HttpResponsePermanentRedirect
+from django.http import HttpResponsePermanentRedirect, HttpResponse
 from django.shortcuts import render
 from appClients.formsClients import NewClients, UpdateClients, SearchClients
+from appClients.Clients import get_values
 
 
-def newClients(request):
-    if request.method == "POST":
-        return HttpResponsePermanentRedirect("/")
-    else:
+def newClients(request):  # добавление нового клиента
+    if request.method == "POST":  # обработка POST запроса
+        userform = NewClients(request.POST)
+        values = get_values(userform)  # данные из формы
+        return HttpResponse(values)
+    else:  # обработка GET запроса
         userform = NewClients()
         return render(request, "form.html", {"form": userform})
 
 
-def updateClients(request):
-    if request.method == "POST":
-        return HttpResponsePermanentRedirect("/")
-    else:
+def updateClients(request):  # обновление информации о клиенте
+    if request.method == "POST":  # обработка POST запроса
+        userform = UpdateClients(request.POST)
+        values = get_values(userform)  # данные из формы
+        return HttpResponse(values)
+    else:  # обработка GET запроса
         userform = UpdateClients()
         return render(request, "form.html", {"form": userform})
 
 
 def searchClients(request):
-    if request.method == "POST":
-        return HttpResponsePermanentRedirect("/")
-    else:
+    if request.method == "POST":  # обработка POST запроса
+        userform = SearchClients(request.POST)
+        values = get_values(userform)  # данные из формы
+        return HttpResponse(values)
+    else:  # обработка GET запроса
         userform = SearchClients()
         return render(request, "form.html", {"form": userform})
